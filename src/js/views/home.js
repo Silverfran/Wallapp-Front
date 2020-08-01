2c1e77688caf6c1e67f8437bef5a6f93800782d0
SilverFran <Francisco1627@gmail.com>
2020-08-01 04:31:37 +0000

    git ignore commit

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -4,8 +4,7 @@ import "../../styles/home.scss";
 export const Home = () => (
 	<div className="text-center mt-5">
 		<h1>Tittle</h1>
-		<p>
-		</p>
+		<p />
 		<a href="#" className="btn btn-success">
 			If you see this green button, bootstrap is working
 		</a>
